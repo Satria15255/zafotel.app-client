217933613b1f0df4c035038400c07f8e10fdcb91
satriazafarani <satriazafarani05@gmail.com>
2025-11-30 10:17:11 +0700

    Update mobile version

--- src/components/InformationsCard.jsx
+++ src/components/InformationsCard.jsx
@@ -4,15 +4,15 @@ const InformationsCard = ({ informations }) => {
     return (
         <div>
             <div className='w-full h-auto'>
-                <div className='grid grid-cols-3 gap-5 place-items-center'>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-5 place-items-center'>
                     {informations.map((info, index) => (
-                        <div key={index} className='w-100 h-100 flex flex-col justify-between rounded-xs border border-gray-300 shadow-lg cursor-pointer'>
+                        <div key={index} className='w-70 md:w-100 h-auto md:w-100 md:h-100 flex flex-col justify-between rounded-xs border border-gray-300 shadow-lg cursor-pointer'>
                             <div className='flex flex-col justify-between'>
-                                <img src={info.image} className='w-100 h-60 object-cover object-center' />
+                                <img src={info.image} className='w-full h-auto object-cover object-center' />
                                 <div className='p-4'>
-                                    <p className='text-sm h-10 font-semibold'>{info.title}</p>
-                                    <p className='text-sm text-gray-500 py-4'>{new Date(info.createdAt).toLocaleDateString()}</p>
-                                    <button onClick={() => navigate(`/informations/${info._id}`)} className='border py-2 px-6 text-xs bg-gray-900 text-white rounded-xs hover:bg-white hover:text-black transition duration-100'>Read More —▸</button>
+                                    <p className='text-xs md:text-sm h-10 font-semibold'>{info.title}</p>
+                                    <p className='text-[10px] md:text-sm text-gray-500 py-4'>{new Date(info.createdAt).toLocaleDateString()}</p>
+                                    <button onClick={() => navigate(`/informations/${info._id}`)} className='border py-2 px-6 text-[10px] md:text-xs bg-gray-900 text-white rounded-xs hover:bg-white hover:text-black transition duration-100'>Read More —▸</button>
                                 </div>
                             </div>
                         </div>
--- src/components/RoomsCard.jsx
+++ src/components/RoomsCard.jsx
@@ -12,35 +12,35 @@ const RoomsCard = ({ rooms }) => {
         <div className='w-auto'>
             <div>
                 {rooms.map((room, index) => (
-                    <div key={index} className='grid grid-cols-2 bg-white p-8 '>
+                    <div key={index} className='grid grid-cols-1 md:grid-cols-2 bg-white p-2 md:p-8 '>
                         <div>
                             <img src={room.image[0]} alt='standardType' />
                         </div>
-                        <div className='p-2 px-6 flex flex-col justify-around'>
-                            <p className='text-lg h-10 text-[#FFD700]'>Start From : <span className='text-2xl'>${room.price}/Night</span></p>
-                            <p className='text-3xl font-serif'>{room.name}</p>
-                            <p className='text-lg font-light max-w-lg'>Enjoy simple comfort with complete amenities in our Standard Room. Designed with a modern and warm feel, this room is ideal for guests who prioritize convenience and comfort while relaxing.</p>
-                            <div className='grid gap-2 grid-cols-2'>
+                        <div className='p-1 md:p-2 px-6 flex flex-col justify-around'>
+                            <p className='text-sm md:text-lg h-10 text-[#FFD700]'>Start From : <span className='text-lg md:text-2xl'>${room.price}/Night</span></p>
+                            <p className='text-lg md:text-3xl font-serif'>{room.name}</p>
+                            <p className='text-sm md:text-lg font-light max-w-lg'>Enjoy simple comfort with complete amenities in our Standard Room. Designed with a modern and warm feel, this room is ideal for guests who prioritize convenience and comfort while relaxing.</p>
+                            <div className='grid gap-2 grid-cols-2 mt-4 md:mt-0'>
                                 <div className='flex gap-1 items-center'>
                                     <LuBedDouble />
-                                    <p>1 King Bed</p>
+                                    <p className='text-[12px]'>1 King Bed</p>
                                 </div>
                                 <div className='flex gap-1 items-center'>
                                     <FaCity />
-                                    <p>Street View</p>
+                                    <p className='text-[12px]'>Street View</p>
                                 </div>
                                 <div className='flex gap-1 items-center'>
                                     <LuUtensils />
-                                    <p>Breakfast</p>
+                                    <p className='text-[12px]'>Breakfast</p>
                                 </div>
                                 <div className='flex gap-1 items-center'>
                                     <TiWiFi />
-                                    <p>WiFi</p>
+                                    <p className='text-[12px]'>WiFi</p>
                                 </div>
                             </div>
-                            <div className='flex gap-4'>
-                                <button className='py-2 px-7 bg-gray-900 border text-white hover:bg-white hover:text-black transition duration-100 flex items-center gap-2'><FaCalendarCheck />Book Now</button>
-                                <button onClick={() => navigate(`/rooms/${room._id}`)} className='py-2 px-7 bg-yellow-300 border text-white hover:bg-white hover:text-black transition duration-100'>View Room →</button>
+                            <div className='flex gap-4 mt-3 md:mt-0'>
+                                <button className='text-[9px] py-1 md:py-2 px-4 md:px-7 bg-gray-900 border text-white hover:bg-white hover:text-black transition duration-100 flex items-center gap-2'><FaCalendarCheck />Book Now</button>
+                                <button onClick={() => navigate(`/rooms/${room._id}`)} className='text-[9px] py-1 md:py-2 px-4 md:px-7 bg-yellow-300 border text-white hover:bg-white hover:text-black transition duration-100'>View Room →</button>
                             </div>
                         </div>
 
